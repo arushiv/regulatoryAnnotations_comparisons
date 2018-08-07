41822e8c6bc793be43685a87967f87b530493949
arushiv <arushiv@umich.edu>
2018-08-07 17:42:47 -0400

    first

---
+++ enrichment_gwas_extended/README.md
@@ -0,0 +1,10 @@
+
+## Make rule graph
+```
+	snakemake -np --configfile config.yaml --rulegraph | dot -Tsvg > rules.svg
+```
+
+# Run
+
+```
+```
---
+++ enrichment_gwas_extended/scripts/plot.R
@@ -0,0 +1,41 @@
+library(ggplot2)
+
+
+args <- commandArgs(TRUE)
+d <- read.table(gzfile(args[1]), header=T, sep='\t')
+
+                                        # eqtl bin job cell annotation feature overlap expected_overlap pval
+
+renameAnnotations <- function(d){
+    d$annotation <- gsub("hotRegions","HOT Regions",d$annotation)
+    d$annotation <- gsub("broadDomains","Broad Domains",d$annotation)
+    d$annotation <- gsub("stretchEnhancer","Stretch Enhancers",d$annotation)
+    d$annotation <- gsub("superEnhancers","Super Enhancers",d$annotation)
+    d$annotation <- gsub("typicalEnhancers","Typical Enhancers",d$annotation)
+    return(d)
+}
+
+makePlot <- function(d){
+    p <- ggplot(d, aes(x=trait, y=Enrich)) +
+        geom_point(aes(fill=annotation), size=2, shape=21, colour="black") +
+        geom_line(aes(group=annotation, colour=annotation)) +
+        labs(y="eQTL Fold enrichment", x="trait") +
+        scale_fill_brewer(name="", palette="Set1") +
+        scale_colour_brewer(name="", palette="Set1") +
+        facet_wrap(~cell, ncol=1) +
+        theme(axis.text.x=element_text(size=7, angle=90, hjust=1, vjust=0.5),
+              strip.text.x = element_text(size = 9),
+              panel.background = element_rect(fill = 'white', colour='black'),
+              panel.grid=element_blank(),
+              legend.position="bottom")
+    return(p)
+}
+
+d <- subset(d, ! annotation %in% c("ActiveTss","typicalEnhancerChromatin") )
+d$Enrich <- (d$overlap/d$expected_overlap)
+
+
+pdf(args[2], height=8, width=25)
+makePlot(renameAnnotations(d))
+dev.off()
+
