e937c6a1f5629eeb4518ec1aef6ea5ec35bae5c7
arushiv <arushiv@umich.edu>
2018-08-28 11:48:48 -0400

    update formatting and plot function

--- summaryStatistics/scripts/plot_jaccard.R
+++ summaryStatistics/scripts/plot_jaccard.R
@@ -27,55 +27,24 @@ renameAnnotations_space <- function(d, colname){
     return(d)
 }
 
-makePlot_across <- function(d){
-    maxE <- max(d$value, na.rm=TRUE )
-    colvector <- c(0, 0.33*maxE, 0.66*maxE, maxE)
-    colvector
-
-    p <- ggplot(d, aes(x = cell1, y = variable)) +
-        geom_tile(aes(fill=value)) +
-        facet_wrap(~feature, nrow=1) +
-        labs(x="Feature 1", y="Feature 2") +
-        scale_fill_viridis(name="Jaccard") +
-        theme(strip.text=element_text(size=7),
+makePlot <- function(d, x=xstring, y=ystring, f=fillstring, xlab=xlab, ylab=ylab, name=name, feature=feature){
+    p <- ggplot(d, aes_string(x = x, y = y)) +
+        geom_tile(aes_string(fill = f)) +
+        facet_wrap(as.formula(paste("~", feature)), nrow=1) +
+        labs(x=xlab, y=ylab) +
+        scale_fill_viridis(name=name, na.value="white") +
+        theme(strip.text=element_text(size=8),
               text = element_text(size=10),
               axis.text.x = element_text(angle=60, hjust=1, size=8),
               axis.text.y = element_text(size=8),
-              panel.background = element_rect(fill="grey", colour="black"),
+              panel.background = element_rect(fill="white", colour="black", size=1),
               panel.grid=element_blank(),
-              legend.position="bottom") 
+              ## axis.line=element_line(colour="black"),
+              strip.background=element_rect(fill="white"))
+    
+              ## legend.position="bottom") 
     return(p)
 }
-
-
-makePlot_same <- function(d){
-    maxE <- max(d$value, na.rm=TRUE )
-    colvector <- c(0, 0.33*maxE, 0.66*maxE, maxE)
-    colvector
-
-    p <- ggplot(d, aes(x = feature1, y = variable)) +
-        geom_tile(aes(fill=value)) +
-        facet_wrap(~cell, nrow=1) +
-        labs(x="Feature 1", y="Feature 2") +
-        scale_fill_viridis(name="Jaccard") +
-        theme(strip.text=element_text(size=7),
-              text = element_text(size=10),
-              axis.text.x = element_text(angle=60, hjust=1, size=8),
-              axis.text.y = element_text(size=8),
-              panel.background = element_rect(fill="grey", colour="black"),
-              panel.grid=element_blank(),
-              legend.position="bottom") 
-    return(p)
-}
-
-
-d$fraction1 <- d$overlap/d$lenght1
-d$fraction2 <- d$overlap/d$length2
-d$jaccard <- d$overlap/(d$lenght1 + d$length2 - d$overlap)
-
-
-# Fix matrix with jaccard for overlaps across annotations within cell types
-samecell <- d[d$cell1 == d$cell2,]
 
 get_lower_tri <- function(cormat){
     cormat[upper.tri(cormat)] <- NA
@@ -87,6 +56,18 @@ get_same_cell_lower_tri <- function(d){
     return(lower_tri)
 }
 
+get_across_cell_lower_tri <- function(d){
+    lower_tri <- get_lower_tri(dcast(d, cell1 ~ cell2, value.var="jaccard"))
+    return(lower_tri)
+}
+
+d$fraction1 <- d$overlap/d$lenght1
+d$fraction2 <- d$overlap/d$length2
+d$jaccard <- d$overlap/(d$lenght1 + d$length2 - d$overlap)
+
+
+# Fix matrix with jaccard for overlaps across annotations within cell types
+samecell <- d[d$cell1 == d$cell2,]
 
 ndf = do.call("rbind", by(samecell, samecell$cell1, get_same_cell_lower_tri))
 ndf <- data.frame(add_rownames(ndf, "cell"))
@@ -96,10 +77,6 @@ plotdf <- melt(ndf)
 plotdf <- renameAnnotations_space(plotdf, "feature1")
 plotdf <- renameAnnotations_space(plotdf, "variable")
 
-get_across_cell_lower_tri <- function(d){
-    lower_tri <- get_lower_tri(dcast(d, cell1 ~ cell2, value.var="jaccard"))
-    return(lower_tri)
-}
 
 # Fix matrix with jaccard for overlaps across cell types within annotations
 diffcell <- d[d$feature1 == d$feature2,]
@@ -113,10 +90,11 @@ plotdfd <- melt(ddf)
 plotdfd <- renameAnnotations(plotdfd, "feature")
 
 
-
+head(plotdf)
 # plot
-pdf(args[2], height=3, width=6)
-makePlot_same(plotdf)
-makePlot_across(plotdfd)
+pdf(args[2], height=2.5, width=7)
+makePlot(plotdf, x="feature1", y="variable", f="value", xlab="Feature 1", ylab="Feature 2", name="Jaccard", feature="cell")
+makePlot(plotdfd, x="cell1", y="variable", f="value", xlab="Feature 1", ylab="Feature 2", name="Jaccard", feature="feature")
 dev.off()
 
+traceback()
