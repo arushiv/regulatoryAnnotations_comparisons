2dae125d666c271ffb377305d0e653d0bd87a48e
arushiv <arushiv@umich.edu>
2018-09-06 14:57:19 -0400

    format newlines

--- summaryStatistics/scripts/heatmap.R
+++ summaryStatistics/scripts/heatmap.R
@@ -24,10 +24,22 @@ makePlot <- function(d){
         geom_tile(aes(fill=fraction1)) +
         facet_grid(feature2~feature1) +
         labs(x="Feature 1", y="Feature 2") +
-        scale_fill_gradientn(colours=c("white","yellow", "red", "darkred"), values=rescale(colvector), breaks=c(colvector), labels=c(round(colvector,2)), name="Intersection/\nlength(Feature 1))") +
-        theme(strip.text=element_text(size=7), text = element_text(size=10), axis.text.x = element_text(angle=90, vjust=0.5, hjust=1, size=8), axis.text.y = element_text(size=8), panel.background = element_rect(fill="grey", colour="black"), panel.grid=element_blank(), legend.position="bottom") 
+        scale_fill_gradientn(colours=c("white","yellow", "red", "darkred"),
+                             values=rescale(colvector),
+                             breaks=c(colvector),
+                             labels=c(round(colvector,2)),
+                             name="Intersection/\nlength(Feature 1))") +
+        theme(strip.text=element_text(size=7),
+              text = element_text(size=10),
+              axis.text.x = element_text(angle=90, vjust=0.5, hjust=1, size=8),
+              axis.text.y = element_text(size=8),
+              panel.background = element_rect(fill="grey", colour="black"),
+              panel.grid=element_blank(),
+              legend.position="bottom") 
     return(p)
 }
+
+
 
 d$fraction1 <- d$overlap/d$lenght1
 d$fraction2 <- d$overlap/d$length2
